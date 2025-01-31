a73d29345de55323356a9dcb2fd30395736ec936
Bharat Paliwal <18uec169@lnmiit.ac.in>
2025-01-31 15:03:35 +0530

    modification

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ import { customAlphabet } from "nanoid";
 const nanoidDigits = customAlphabet('0123456789', 6);
 
 const generateUID = (prefix = "") => {
-  const now = new Date();
-  const dd = String(now.getDate()).padStart(2, '0');
-  const mm = String(now.getMonth() + 1).padStart(2, '0');
-  const yyyy = now.getFullYear();
-  const hh = String(now.getHours()).padStart(2, '0');
-  const min = String(now.getMinutes()).padStart(2, '0');
-  const ss = String(now.getSeconds()).padStart(2, '0');
+  // const now = new Date();
+  // const dd = String(now.getDate()).padStart(2, '0');
+  // const mm = String(now.getMonth() + 1).padStart(2, '0');
+  // const yyyy = now.getFullYear();
+  // const hh = String(now.getHours()).padStart(2, '0');
+  // const min = String(now.getMinutes()).padStart(2, '0');
+  // const ss = String(now.getSeconds()).padStart(2, '0');
 
-  const timestamp = `${dd}${mm}${yyyy}${hh}${min}${ss}`; // ddmmyyyyhhmmss
+  const timestamp = new Date().toISOString().replace(/\D/g, '').slice(2, 14);
+  // const timestamp = `${dd}${mm}${yyyy}${hh}${min}${ss}`; // ddmmyyyyhhmmss
   const randomPart = nanoidDigits(); // 6-digit unique number
   return `${prefix}-${timestamp}-${randomPart}`;
 }
